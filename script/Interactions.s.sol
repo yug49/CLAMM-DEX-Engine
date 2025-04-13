34ee274279c221d6ebeeaead09ced74d69bc79e1
yug49 <148035793+yug49@users.noreply.github.com>
2025-04-13 17:25:47 +0530

    Update Interactions.s.sol

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -12,8 +12,8 @@ import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
  */
 contract AddLiquidity is Script {
     function run() public {
-        int24 tickLower = int24(uint24(vm.envUint("LOWER_TICK")));
-        int24 tickUpper = int24(uint24(vm.envUint("UPPER_TICK")));
+        int24 tickLower = int24(vm.envInt("LOWER_TICK"));
+        int24 tickUpper = int24(vm.envInt("UPPER_TICK"));
         uint128 amount = uint128(vm.envUint("AMOUNT_TO_ADD"));
 
         address recentDeployment = DevOpsTools.get_most_recent_deployment("CLAMMPool", block.chainid);
@@ -28,8 +28,8 @@ contract AddLiquidity is Script {
 
 contract RemoveLiquidity is Script {
     function run() public {
-        int24 tickLower = int24(uint24(vm.envUint("LOWER_TICK")));
-        int24 tickUpper = int24(uint24(vm.envUint("UPPER_TICK")));
+        int24 tickLower = int24(vm.envInt("LOWER_TICK"));
+        int24 tickUpper = int24(vm.envInt("UPPER_TICK"));
         uint128 amount = uint128(vm.envUint("AMOUNT_TO_REMOVE"));
 
         address recentDeployment = DevOpsTools.get_most_recent_deployment("CLAMMPool", block.chainid);
@@ -47,8 +47,8 @@ contract RemoveLiquidity is Script {
  */
 contract CollectFeesAndRemovedLiquidity is Script {
     function run() public {
-        int24 tickLower = int24(uint24(vm.envUint("LOWER_TICK")));
-        int24 tickUpper = int24(uint24(vm.envUint("UPPER_TICK")));
+        int24 tickLower = int24(vm.envInt("LOWER_TICK"));
+        int24 tickUpper = int24(vm.envInt("UPPER_TICK"));
         uint128 amount0Requested = uint128(vm.envUint("AMOUNT0_TO_COLLECT"));
         uint128 amount1Requested = uint128(vm.envUint("AMOUNT1_TO_COLLECT"));
 
